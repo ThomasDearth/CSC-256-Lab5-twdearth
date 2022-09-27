164536b23be79e07bea43aa9c0cf760640551f60
Thomas Dearth <twdearth@my.waketech.edu>
2022-09-27 19:12:52 -0400

    Makes whats_my_name.py less useful

--- gitremote/test/unit/test_whats_my_name.py
+++ gitremote/test/unit/test_whats_my_name.py
@@ -4,4 +4,4 @@ from gitremote.whats_my_name import my_name_is
 
 
 def test_my_name_is():
-    assert "twdearth@waketech.edu" == my_name_is()
+    assert "the return of my_name_is()" == my_name_is()
--- gitremote/whats_my_name.py
+++ gitremote/whats_my_name.py
@@ -1,5 +1,5 @@
 def my_name_is():
-    return "twdearth@waketech.edu"
+    return "the return of my_name_is()"
 
 
 # palaheel76 doesn't want any comments
